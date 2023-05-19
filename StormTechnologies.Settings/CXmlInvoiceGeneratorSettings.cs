1a23adc368b894a89cfecae92335568b7b5e94d8
saeed-nawaz <nawaz.saeed@gmail.com>
2023-05-19 08:04:35 +0100

    Add files via upload

---
+++ StormTechnologies.Settings/CXmlInvoiceGeneratorSettings.cs
@@ -0,0 +1,16 @@
+﻿/*
+ Written by Saeed Nawaz (May-2023) for the Storm Technologies Technical Test.
+*/
+namespace StormTechnologies.Settings
+{
+    /// <summary>
+    /// Grouped settings From, To and Sender CXML elements
+    /// </summary>
+    public class CXmlInvoiceGeneratorSettings
+    {
+        public string Domain { get; set; }
+        public string Identity { get; set; }
+        public string SharedSecret { get; set; }
+        public string UserAgent { get; set; }
+    }
+}
---
+++ StormTechnologies.Settings/CXmlInvoiceGeneratorSettingsContext.cs
@@ -0,0 +1,22 @@
+﻿/*
+ Written by Saeed Nawaz (May-2023) for the Storm Technologies Technical Test.
+*/
+namespace StormTechnologies.Settings
+{
+    /// <summary>
+    /// Settings for CXML invoice generator
+    /// </summary>
+    public class CXmlInvoiceGeneratorSettingsContext
+    {
+        public string OutputFilePath { get; set; }
+        public string PayloadId { get; set; }
+        public string DeploymentMode { get; set; }
+        public string Operation { get; set; }
+        public string Purpose { get; set; }
+        public string Language { get; set; }
+
+        public CXmlInvoiceGeneratorSettings From { get; set; }
+        public CXmlInvoiceGeneratorSettings To { get; set; }
+        public CXmlInvoiceGeneratorSettings Sender { get; set; }
+    }
+}
